a4dfa8bd81e0aacd86dd617cfd11d5f8a9eed5b6
Shea Underwood <shea12underwood@gmail.com>
2019-08-05 09:10:44 -0400

    Working smoothly, goes to 100 day if 200 day isn't available.

--- MovingAverages.py
+++ MovingAverages.py
@@ -11,35 +11,36 @@ driver = webdriver.Chrome(chrome_path)
 
 
 def price2MA(alist):
-    alist = set(alist)
-    ticklist = []
-    MAlist = []
-    pricelist = []
+    alist = set(alist)    
     for tick in alist:
         URL = "https://www.barchart.com/stocks/quotes/" +tick+"/technical-analysis"
         driver.get(URL)
-
         try:
             MA = driver.find_element_by_xpath("""//*[@id="main-content-column"]/div/div[2]/div/div[1]/div/div/ng-transclude/table/tbody/tr[5]/td[2]""")
-            MAlist.append(MA.text)
+            Length = "200 day"
+            if MA.text == "N/A":
+                print("200 day not found for "+tick+ ", moving to 100 day")
+                MA = driver.find_element_by_xpath("""//*[@id="main-content-column"]/div/div[2]/div/div[1]/div/div/ng-transclude/table/tbody/tr[4]/td[2]""")
+                Length = "100 day"                 
         except:
+            print("continued at MA" + tick)
             continue
 
         try:
-            price = driver.find_element_by_xpath("""//*[@id="main-content-column"]/div/div[1]/div[3]/div[2]/span[1]""")
-            pricelist.append(price.text)
-            ticklist.append(tick)
+            price = driver.find_element_by_xpath("""//*[@id="main-content-column"]/div/div[1]/div[3]/div[3]/span[1]""")
+            
         except:
+            print("continued at price" + tick)
             continue
 
         try:
-            percent(tick,price.text,MA.text)
+            percent(tick,price.text,MA.text,Length)
+            
         except:
-            print("something went wrong, continuing")
+            print("continued at percent" + tick)
             continue
-
-    
-def percent(Ticker, Price, MA):
+    driver.close()
+def percent(Ticker, Price, MA, Length):
     if MA == 'N/A':
         movA = float(1)
     else:
@@ -50,7 +51,7 @@ def percent(Ticker, Price, MA):
         aorb = "above"
     else:
         aorb = "below"
-    print(Ticker +" is trading at " +str(round(result,2)).replace("-","")+"% " +aorb +" the 200 day moving average")
+    print(Ticker +" is trading at " +str(round(result,2)).replace("-","")+"% " +aorb +" the "+ Length+" moving average")
 
 
 price2MA(alist)
