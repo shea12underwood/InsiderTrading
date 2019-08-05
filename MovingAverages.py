a54b3a88eb21a7d6344ec0f8ae1a1711461ae941
Shea Underwood <shea12underwood@gmail.com>
2019-08-05 14:20:19 -0400

    added a report of the RSI, EMA, and MacD

--- MovingAverages.py
+++ MovingAverages.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 import TradingWithCrossRef as Tr
+import pandas_datareader as dr
 import pandas as pd
+import utils4stocks as utils
 
-alist = Tr.getBuys()
+# alist = Tr.getBuys()
 
 chrome_path = r"C:\Users\u15866\OneDrive - Kimberly-Clark\chromedriver.exe"
 driver = webdriver.Chrome(chrome_path)
 
-# alist = ["ETRN","FLLCU","FLLCU","GRA","USAK",]
+alist = ["ETRN", "spxu"]
 
 
 def price2MA(alist):
@@ -18,20 +20,24 @@ def price2MA(alist):
         try:
             MA = driver.find_element_by_xpath("""//*[@id="main-content-column"]/div/div[2]/div/div[1]/div/div/ng-transclude/table/tbody/tr[5]/td[2]""")
             Length = "200 day"
+            Lengthforcalc = 200
             if MA.text == "N/A":
                 print("200 day not found for "+tick+ ", moving to 100 day")
                 MA = driver.find_element_by_xpath("""//*[@id="main-content-column"]/div/div[2]/div/div[1]/div/div/ng-transclude/table/tbody/tr[4]/td[2]""")
-                Length = "100 day"                 
+                Length = "100 day" 
+                Lengthforcalc = 100                
         except:
             print("continued at MA" + tick)
             continue
 
         try:
-            price = driver.find_element_by_xpath("""//*[@id="main-content-column"]/div/div[1]/div[3]/div[3]/span[1]""")
-            
+            price = driver.find_element_by_xpath("""//*[@id="main-content-column"]/div/div[1]/div[3]/div[3]/span[1]""")           
         except:
-            print("continued at price" + tick)
-            continue
+            try: 
+                price = driver.find_element_by_xpath("""//*[@id="main-content-column"]/div/div[1]/div[3]/div[2]/span[1]""")
+            except:
+                print("continued at price" + tick)
+                continue
 
         try:
             percent(tick,price.text,MA.text,Length)
@@ -39,6 +45,14 @@ def price2MA(alist):
         except:
             print("continued at percent" + tick)
             continue
+        df = dr.data.get_data_yahoo(tick,start = '2018-08-27', end = '2019-08-05')
+        df = df.reset_index()
+        df = utils.relative_strength_index(df,14)
+        df = utils.exponential_moving_average(df,180)
+        df = utils.moving_average(df,Lengthforcalc)
+        df = utils.macd(df,12,26)
+        print(df[-5:])
+
     driver.close()
 def percent(Ticker, Price, MA, Length):
     if MA == 'N/A':
---
+++ utils4stocks.py
@@ -0,0 +1,80 @@
+import pandas as pd 
+import pandas_datareader as dr
+
+def moving_average(df, n):
+    """Calculate the moving average for the given data.
+    
+    :param df: pandas.DataFrame
+    :param n: 
+    :return: pandas.DataFrame
+    """
+    MA = pd.Series(df['Close'].rolling(n, min_periods=n).mean(), name='MA_' + str(n))
+    df = df.join(MA)
+    return df
+
+def exponential_moving_average(df, n):
+    """
+    
+    :param df: pandas.DataFrame
+    :param n: 
+    :return: pandas.DataFrame
+    """
+    EMA = pd.Series(df['Close'].ewm(span=n, min_periods=n).mean(), name='EMA_' + str(n))
+    df = df.join(EMA)
+    return df
+
+def macd(df, n_fast, n_slow):
+    """Calculate MACD, MACD Signal and MACD difference
+    
+    :param df: pandas.DataFrame
+    :param n_fast: 
+    :param n_slow: 
+    :return: pandas.DataFrame
+    """
+    EMAfast = pd.Series(df['Close'].ewm(span=n_fast, min_periods=n_slow).mean())
+    EMAslow = pd.Series(df['Close'].ewm(span=n_slow, min_periods=n_slow).mean())
+    MACD = pd.Series(EMAfast - EMAslow, name='MACD_' + str(n_fast) + '_' + str(n_slow))
+    MACDsign = pd.Series(MACD.ewm(span=9, min_periods=9).mean(), name='MACDsign_' + str(n_fast) + '_' + str(n_slow))
+    MACDdiff = pd.Series(MACD - MACDsign, name='MACDdiff_' + str(n_fast) + '_' + str(n_slow))
+    df = df.join(MACD)
+    df = df.join(MACDsign)
+    df = df.join(MACDdiff)
+    return df
+
+def relative_strength_index(df, n):
+    """Calculate Relative Strength Index(RSI) for given data.
+    
+    :param df: pandas.DataFrame
+    :param n: 
+    :return: pandas.DataFrame
+    """
+    i = 0
+    UpI = [0]
+    DoI = [0]
+    while i + 1 <= df.index[-1]:
+        UpMove = df.loc[i + 1, 'High'] - df.loc[i, 'High']
+        DoMove = df.loc[i, 'Low'] - df.loc[i + 1, 'Low']
+        if UpMove > DoMove and UpMove > 0:
+            UpD = UpMove
+        else:
+            UpD = 0
+        UpI.append(UpD)
+        if DoMove > UpMove and DoMove > 0:
+            DoD = DoMove
+        else:
+            DoD = 0
+        DoI.append(DoD)
+        i = i + 1
+    UpI = pd.Series(UpI)
+    DoI = pd.Series(DoI)
+    PosDI = pd.Series(UpI.ewm(span=n, min_periods=n).mean())
+    NegDI = pd.Series(DoI.ewm(span=n, min_periods=n).mean())
+    RSI = pd.Series(PosDI / (PosDI + NegDI), name='RSI_' + str(n))
+    df = df.join(RSI)
+    return df
+
+if __name__ == "__main__":
+    moving_average()
+    exponential_moving_average()
+    macd()
+    relative_strength_index()
